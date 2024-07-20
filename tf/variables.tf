49949b18f9a628dca7480f82edc1df886d0868ee
Biarys <sbiarys@gmail.com>
2024-07-20 17:23:18 -0400

    changed prohect name to use default project

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,11 +1,11 @@
 variable "project_id" {
     description = "Id of the GCP Project"
     type = string
-    default = "gcp-practice-test-1"
+    default = "confident-key-429822-i1"
 }
 
 variable "project_name" {
     description = "Name of the GCP Project"
     type = string
-    default = "gcp-practice"
+    default = "My First Project"
 }
