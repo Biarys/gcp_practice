e408d471b1f62b4ddeffd5be7aa2662429760211
Biarys <sbiarys@gmail.com>
2024-07-20 21:51:10 -0400

    added rng generator tf code

--- tf/main.tf
+++ tf/main.tf
@@ -11,3 +11,52 @@ provider "google" {
   project = var.project_id
   region = var.project_region
 }
+
+resource "google_storage_bucket" "default" {
+  name                        = "gcp-practice-cfs-gcf-source" # Every bucket name must be globally unique
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+data "archive_file" "cf_rng_archive" {
+  type        = "zip"
+  output_path = "/tmp/function-random-number-gen.zip"
+  source_dir  = "../function_random_number_gen/"
+}
+
+resource "google_storage_bucket_object" "object_rng" {
+  name   = "function-random-number-gen.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.cf_rng_archive.output_path # Add path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "cf_rng" {
+  name        = "gen-random-num-function-v2"
+  location    = var.project_region
+  description = "Generate random number function"
+
+  build_config {
+    runtime     = "python312"
+    entry_point = "randomgen" # Set the entry point
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.object_rng.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    available_memory   = "256M"
+    timeout_seconds    = 60
+  }
+}
+
+resource "google_cloud_run_service_iam_member" "member" {
+  location = google_cloudfunctions2_function.cf_rng.location
+  service  = google_cloudfunctions2_function.cf_rng.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
+
--- tf/variables.tf
+++ tf/variables.tf
@@ -15,3 +15,11 @@ variable "project_region" {
     type = string
     default = "us-east1"
 }
+
+output "multiply_function_uri" {
+    value = google_cloudfunctions2_function.cf_rng.service_config[0].uri
+}
+
+output "gen_random_number_function_uri" {
+    value = google_cloudfunctions2_function.cf_rng.service_config[0].uri
+}
