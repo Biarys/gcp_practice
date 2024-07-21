87f974c1ade4463d87eab5fcb05343c62c36b7e4
Biarys <sbiarys@gmail.com>
2024-07-21 13:23:15 -0400

    moved outputs from var to output file

--- tf/outputs.tf
+++ tf/outputs.tf
@@ -2,3 +2,11 @@ output "google_project_id" {
   description = "Google Project Id"
   value       = "Current GCP project is set to ${var.project_id}"
 }
+
+output "multiply_function_uri" {
+    value = google_cloudfunctions2_function.cf_rng.service_config[0].uri
+}
+
+output "gen_random_number_function_uri" {
+    value = google_cloudfunctions2_function.cf_rng.service_config[0].uri
+}
